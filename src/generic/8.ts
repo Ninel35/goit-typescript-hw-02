d536021f79f30b75ebf8ea528a96486f40d4bf1b
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2024-02-01 21:54:53 +0100

    work

--- src/generic/8.ts
+++ src/generic/8.ts
@@ -19,6 +19,9 @@ type Form = {
 };
 
 // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
+type Params = Form & {
+  errors: Partial<Record<keyof Form, [string]>>;
+}
 
 export {};
+
